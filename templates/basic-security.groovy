7f4a0967f88c49904121f3b5c092374c6f0881c8
Loïc Ortola <contact@loicortola.com>
2016-05-16 18:56:03 +0200

    Fixed Jenkins 2.0 issues

---
+++ templates/basic-security.groovy
@@ -0,0 +1,16 @@
+#!groovy
+
+import jenkins.model.*
+import hudson.security.*
+
+def instance = Jenkins.getInstance()
+
+println "--> creating local user 'admin'"
+
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+hudsonRealm.createAccount('{{ jenkins_admin.username }}','{{ jenkins_admin.password }}')
+instance.setSecurityRealm(hudsonRealm)
+
+def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
+instance.setAuthorizationStrategy(strategy)
+instance.save()
