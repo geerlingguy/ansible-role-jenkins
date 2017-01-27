de5b647cbf6506395b500e8418af6027174ec5d1
Manjit Kumar <manjit1727@gmail.com>
2017-01-27 14:38:38 +0530

    rebase with geerlingguy-master

---
+++ templates/basic-security.groovy
@@ -0,0 +1,19 @@
+#!groovy
+import hudson.security.*
+import jenkins.model.*
+
+def instance = Jenkins.getInstance()
+
+println "--> Checking if security has been set already"
+
+if (instance.isUseSecurity()) {
+    println "--> creating local user 'admin'"
+
+    def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+    hudsonRealm.createAccount('{{ jenkins_admin_username }}', '{{ jenkins_admin_password }}')
+    instance.setSecurityRealm(hudsonRealm)
+
+    def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
+    instance.setAuthorizationStrategy(strategy)
+    instance.save()
+}
