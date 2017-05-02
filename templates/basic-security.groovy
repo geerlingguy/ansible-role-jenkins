6fc17b7044a36e8a9263dbd8ed39bc728c51f0a3
Jeff Geerling <geerlingguy@mac.com>
2017-05-02 11:28:02 -0500

    Fixes #79, Fixes #116: Set groovy script to work even when Jenkins is started automatically.

--- templates/basic-security.groovy
+++ templates/basic-security.groovy
@@ -3,13 +3,17 @@ import hudson.security.*
 import jenkins.model.*
 
 def instance = Jenkins.getInstance()
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+def users = hudsonRealm.getAllUsers()
+users_s = users.collect { it.toString() }
 
-println "--> Checking if security has been set already"
+// Create the admin user account if it doesn't already exist.
+if ("{{ jenkins_admin_username }}" in users_s) {
+    println "Admin user already exists"
+}
+else {
+    println "--> creating local admin user"
 
-if (!instance.isUseSecurity()) {
-    println "--> creating local user 'admin'"
-
-    def hudsonRealm = new HudsonPrivateSecurityRealm(false)
     hudsonRealm.createAccount('{{ jenkins_admin_username }}', '{{ jenkins_admin_password }}')
     instance.setSecurityRealm(hudsonRealm)
 
