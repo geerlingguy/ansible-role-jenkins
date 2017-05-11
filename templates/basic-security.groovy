c9e043335a3f2b61c48d27a173c5b3975f07b6fa
Jeff Geerling <geerlingguy@mac.com>
2017-05-11 14:12:47 -0500

    Fixes #136: Force update jenkins_admin_password if account created at install.

--- templates/basic-security.groovy
+++ templates/basic-security.groovy
@@ -9,7 +9,12 @@ users_s = users.collect { it.toString() }
 
 // Create the admin user account if it doesn't already exist.
 if ("{{ jenkins_admin_username }}" in users_s) {
-    println "Admin user already exists"
+    println "Admin user already exists - updating password"
+
+    def user = hudson.model.User.get('{{ jenkins_admin_username }}');
+    def password = hudson.security.HudsonPrivateSecurityRealm.Details.fromPlainPassword('{{ jenkins_admin_password }}')
+    user.addProperty(password)
+    user.save()
 }
 else {
     println "--> creating local admin user"
